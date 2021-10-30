6df18ec1ca1f2412970de338eaea25c08ee0c6a6
soyboy <sbvegan@protonmail.com>
2021-10-30 16:10:21 -0700

    using mock contract

--- test/unit/MyFirstContract_unit_test.js
+++ test/unit/MyFirstContract_unit_test.js
@@ -1,9 +1,20 @@
 const { expect } = require('chai');
 
+const chai = require('chai');
+const BN = require('bn.js');
+chai.use(require('chai-bn')(BN))
+
+const DECIMALS='18'
+const INITIAL_PRICE='2000000000000000000000'
+
 describe('MyFirstContract Unit Test', () => {
     before(async () => {
+        MockV3Aggregator = await ethers.getContractFactory('MockV3Aggregator')
+        mockV3Aggregator = await MockV3Aggregator.deploy(DECIMALS, INITIAL_PRICE)
+        await mockV3Aggregator.deployed()
+        
         MyFirstContract = await ethers.getContractFactory('MyFirstContract')
-        myFirstContract = await MyFirstContract.deploy()
+        myFirstContract = await MyFirstContract.deploy(mockV3Aggregator.address)
         await myFirstContract.deployed()
     })
 
@@ -20,4 +31,9 @@ describe('MyFirstContract Unit Test', () => {
         expect((await myFirstContract.getNumber()).toString()).to.equal('33')
     })
 
+    it('gets a price feed value', async () => {
+        let result = await myFirstContract.getLatestPrice()
+        console.log('price: ' + new ethers.BigNumber.from(result._hex).toString())
+        expect((new ethers.BigNumber.from(result._hex).toString())).equal(INITIAL_PRICE).toString()
+    })
 })
